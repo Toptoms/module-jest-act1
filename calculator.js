acb0e35b82f4d25eeffa260aba74bb95462317ca
Toptom47 <thomasbozon@gmail.com>
2020-03-30 12:36:47 +0200

    testDateTime

--- calculator.js
+++ calculator.js
@@ -1,5 +1,14 @@
 module.exports = {
-    sum: (a,b) => a+b,
-    div: (a,b) => a/b
+    sum: (a, b) => a + b,
+    div: (a, b) => a / b,
+    multi: (a, b) => a * b,
+    soust: (a, b) => a - b,
+
+    dateTime2000: () => {
+        let testTime = new Date("2000-01-01T00:00:00").getTime()
+        let nowTime = Date.now()
+        return Math.abs(testTime - nowTime) / 1000
+    }
+
 }
 
--- calculator.test.js
+++ calculator.test.js
@@ -1,6 +1,39 @@
-const {sum, div} = require('./calculator');
+const { sum, div, multi, soust, dateTime2000 } = require('./calculator');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
-    expect(div(1,1)).toBe(1);
+
+});
+
+test(' 1 / 1 to equal 1', () => {
+    expect(div(1, 1)).toBe(1);
+
+});
+
+test(' 1 * 1 to equal 1', () => {
+    expect(multi(1, 1)).toBe(1);
+
+
+});
+
+test(' 1 - 1 to equal 0', () => {
+    expect(soust(1, 1)).toBe(0);
+
+});
+
+describe('dateTime2000', () => {
+    let originalDateNow = Date.now;
+
+    beforeEach(() => {
+        Date.now = () => new Date("2000-01-01T00:10:00").getTime()
+    });
+
+    afterEach(() => {
+        Date.now = originalDateNow
+    });
+
+    test('dateTime2000', () => {       
+        expect(dateTime2000()).toBe(600)
+    });
+
 });
